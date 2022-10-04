c05c68a20b879577316fb54eae822e56eebe357a
SebuZZZv <110625365+SebuZZZv@users.noreply.github.com>
2022-10-04 21:34:21 +0200

    Second Commit

--- script.js
+++ script.js
@@ -1,176 +1,176 @@
 function Structuremerc() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Mercury-Structure.png")){
+    if (image.src.match("assets/Mercury-Structure.png")){
 
     }
     else {
-        image.src = "image/Mercury-Structure.png";
+        image.src = "assets/Mercury-Structure.png";
         
     }
 }
 function Structurevenus() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Venus-Structure.png")){
+    if (image.src.match("assets/Venus-Structure.png")){
 
     }
     else {
-        image.src = "image/Venus-Structure.png";
+        image.src = "assets/Venus-Structure.png";
         
     }
 }
 function Structureearth() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Earth-Structure.png")){
+    if (image.src.match("assets/Earth-Structure.png")){
 
     }
     else {
-        image.src = "image/Earth-Structure.png";
+        image.src = "assets/Earth-Structure.png";
         
     }
 }
 function Structuremars() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Mars-Structure.png")){
+    if (image.src.match("assets/Mars-Structure.png")){
 
     }
     else {
-        image.src = "image/Mars-Structure.png";
+        image.src = "assets/Mars-Structure.png";
         
     }
 }
 function Structurejupiter() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Jupiter-Structure.png")){
+    if (image.src.match("assets/Jupiter-Structure.png")){
 
     }
     else {
-        image.src = "image/Jupiter-Structure.png";
+        image.src = "assets/Jupiter-Structure.png";
         
     }
 }
 function Structuresaturn() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Saturn-Structure.png")){
+    if (image.src.match("assets/Saturn-Structure.png")){
 
     }
     else {
-        image.src = "image/Saturn-Structure.png";
+        image.src = "assets/Saturn-Structure.png";
         
     }
 }
 function Structureuranus() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Uranus-Structure.png")){
+    if (image.src.match("assets/Uranus-Structure.png")){
 
     }
     else {
-        image.src = "image/Uranus-Structure.png";
+        image.src = "assets/Uranus-Structure.png";
         
     }
 }
 function Structureneptune() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh.';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Neptune-Structure.png")){
+    if (image.src.match("assets/Neptune-Structure.png")){
 
     }
     else {
-        image.src = "image/Neptune-Structure.png";
+        image.src = "assets/Neptune-Structure.png";
         
     }
 }
 function Overviewmerc() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/merc.png")){
+    if (image.src.match("assets/merc.png")){
 
     }
     else {
-        image.src = "image/merc.png";
+        image.src = "assets/merc.png";
         
     }
 }
 function Overviewvenus() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Venus.png")){
+    if (image.src.match("assets/Venus.png")){
 
     }
     else {
-        image.src = "image/Venus.png";
+        image.src = "assets/Venus.png";
         
     }
 }
 function Overviewearth() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Earth1.png")){
+    if (image.src.match("assets/Earth1.png")){
 
     }
     else {
-        image.src = "image/Earth1.png";
+        image.src = "assets/Earth1.png";
         
     }
 }
 function Overviewmars() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Mars.png")){
+    if (image.src.match("assets/Mars.png")){
 
     }
     else {
-        image.src = "image/Mars.png";
+        image.src = "assets/Mars.png";
         
     }
 }
 function Overviewjupiter() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Jupiter1.png")){
+    if (image.src.match("assets/Jupiter1.png")){
 
     }
     else {
-        image.src = "image/Jupiter1.png";
+        image.src = "assets/Jupiter1.png";
         
     }
 }
 function Overviewsaturn() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Saturn.png")){
+    if (image.src.match("assets/Saturn.png")){
 
     }
     else {
-        image.src = "image/Saturn.png";
+        image.src = "assets/Saturn.png";
         
     }
 }
 function Overviewuranus() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Uranus.png")){
+    if (image.src.match("assets/Uranus.png")){
 
     }
     else {
-        image.src = "image/Uranus.png";
+        image.src = "assets/Uranus.png";
         
     }
 }
 function Overviewneptune() {
     document.querySelector(".desc").innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel fringilla est ullamcorper eget nulla facilisi etiam. Nam aliquam sem et tortor consequat id porta nibh venenatis Ac felis donec et odio pellentesque. Blandit cursus risus at ultrices mi. Diam maecenas ultricies mi eget mauris pharetra et. Egestas diam in arcu cursus euismod quis viverra nibh. Risus pretium quam vulputate dignissim suspendisse in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Semper viverra nam libero justo laoreet sit amet. Vestibulum lectus mauris ultrices eros in cursus. ';
     var image = document.getElementById('myImage');
-    if (image.src.match("image/Neptune.png")){
+    if (image.src.match("assets/Neptune.png")){
 
     }
     else {
-        image.src = "image/Neptune.png";
+        image.src = "assets/Neptune.png";
         
     }
 }
